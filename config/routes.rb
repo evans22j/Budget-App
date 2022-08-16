7ec3726852fcfaec5cfe65f2462f3956cbd20570
Evans Sitibekiso <jevacrism@gmail.com>
2022-08-17 01:10:26 +0200

    Initial setup

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :payments
+  resources :categories
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "categories#index"
+end
