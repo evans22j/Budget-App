cf96b523b9e56b10d540972a673837a33acf6f8c
sashinde8770 <sashinde8770@gmail.com>
2023-12-26 16:25:12 +0530

    entrypoint edited

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,10 @@
-  GNU nano 6.2                                                                                   entrypoint.sh                                                                                             
 #!/bin/bash
-set -e
 
 # If running the rails server then create or migrate existing database
-if [ "${*}" == "./bin/rails server" ]; then
+if [ "${*}" = "./bin/rails server" ]; then
   ./bin/rails db:create
   ./bin/rails db:prepare
 fi
-
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
